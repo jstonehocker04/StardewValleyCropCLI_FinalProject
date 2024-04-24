4efad1e2041289b3a89a37e8266995604b170028
jston <jston@YEAGER-CLASS>
2024-04-24 15:48:23 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+/*----------------------------------------------------
+ * Program Title:	Stardew Valley Crop Information
+ * Author:          Josiah Stonehocker
+ * Date:			4/24/2024
+ * Description:
+ * Usage / Issues:
+ *-----------------------------------------------------
+ */
