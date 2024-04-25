aa4e80f9f79b1f7027a359ca599e04212e9d9e96
Josiah Stonehocker <jstonehocker04@gmail.com>
2024-04-24 20:05:12 -0500

    Created CropInfo.csv and TestCropInfo.csv. Made Title.txt that stores ascii art to be printed out.

--- Main.cpp
+++ Main.cpp
@@ -6,3 +6,20 @@
  * Usage / Issues:
  *-----------------------------------------------------
  */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int main() {
+	std::ifstream input_file;
+	//	Print's ascii art title
+	input_file.open("Title.txt");
+	if (input_file) {
+		std::string line;
+		while (getline(input_file, line))
+			std::cout << line << '\n';
+		input_file.close();
+	}
+}
